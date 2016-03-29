09db10d6ce6eba7a807bf8725d0bf9656e8bc4a8
Coderhypo <i@ihypo.net>
2016-03-30 02:51:56 +0800

    add requests

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or 'FUCKTHEGRADEPOINT'
 
+APPID = os.getenv('APPID') or 'wx64a1e9e7a16828da'
+APPSECRET = os.getenv('APPSECRET') or 'fd5712b4ca3ff65f6414f9953d370f15'
+
 import index
