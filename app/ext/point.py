8d3b54b6434af711af4a077e258f070482d35993
Coderhypo <i@ihypo.net>
2016-03-30 20:45:01 +0800

    fix

--- app/ext/point.py
+++ app/ext/point.py
@@ -2,13 +2,13 @@
 # coding=utf-8
 
 import json
-import request
+import requests
 
 def get_point(user):
     no = user['no']
     URL = "http://gpa.updev.cn/api"
 
-    req = request.post(URL, data={'no': no})
+    req = requests.post(URL, data={'no': no})
     userinfo = json.loads(req.text)
 
     return userinfo
